9b4b346bfca571d53213ac6e94053689eba0b01a
Swanand Shende <sshende@catalogicsoftware.com>
2024-09-08 10:43:05 +0530

    works - 3

--- internal/plugin/restoreplugin.go
+++ internal/plugin/restoreplugin.go
@@ -21,14 +21,14 @@ import (
 	"github.com/vmware-tanzu/velero/pkg/plugin/velero"
 )
 
-// RestorePlugin is a restore item action plugin for Velero
-type RestorePlugin struct {
+// RestoreFilterPlugin is a restore item action plugin for Velero
+type RestoreFilterPlugin struct {
 	log logrus.FieldLogger
 }
 
 // NewRestorePlugin instantiates a RestorePlugin.
-func NewRestorePlugin(log logrus.FieldLogger) *RestorePlugin {
-	return &RestorePlugin{log: log}
+func NewRestorePlugin(log logrus.FieldLogger) *RestoreFilterPlugin {
+	return &RestoreFilterPlugin{log: log}
 }
 
 // AppliesTo returns information about which resources this action should be invoked for.
@@ -36,13 +36,13 @@ func NewRestorePlugin(log logrus.FieldLogger) *RestorePlugin {
 // and resources with group names. These work: "ingresses", "ingresses.extensions".
 // A RestoreItemAction's Execute function will only be invoked on items that match the returned
 // selector. A zero-valued ResourceSelector matches all resources.
-func (p *RestorePlugin) AppliesTo() (velero.ResourceSelector, error) {
+func (p *RestoreFilterPlugin) AppliesTo() (velero.ResourceSelector, error) {
 	return velero.ResourceSelector{}, nil
 }
 
 // Execute allows the RestorePlugin to perform arbitrary logic with the item being restored,
 // in this case, setting a custom annotation on the item being restored.
-func (p *RestorePlugin) Execute(input *velero.RestoreItemActionExecuteInput) (*velero.RestoreItemActionExecuteOutput, error) {
+func (p *RestoreFilterPlugin) Execute(input *velero.RestoreItemActionExecuteInput) (*velero.RestoreItemActionExecuteOutput, error) {
 	p.log.Info("Hello from my Cloudcasa Restore Filter Plugin!")
 	return velero.NewRestoreItemActionExecuteOutput(input.Item), nil
 }
